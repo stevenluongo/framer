9c090d356512b2abec3068c68f28ec8705d82a7f
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-11-29 13:16:04 +0000

    add whileInView animations

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Input } from "./Input";
+
+
+
+function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [rotate, setRotate] = useState(0);
+  return (
+    <div>
+      <div className="bg-[red] h-[100vh]">
+        <motion.h1 className="text-white text-5xl"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+        >Hello world</motion.h1>
+      <motion.div
+          className="bg-[green] w-[200px] h-[200px]"
+          animate={{ x, y, rotate }}
+          transition={{ type: "spring" }}
+        />
+              <div className="inputs">
+        <Input value={x} set={setX}>
+          x
+        </Input>
+        <Input value={y} set={setY}>
+          y
+        </Input>
+        <Input value={rotate} set={setRotate} min={-180} max={180}>
+          rotate
+        </Input>
+      </div>
+      </div>
+      <div className="bg-[blue] h-[100vh]">
+      <motion.h1 className="text-white text-5xl"
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1  }}
+          transition={{ type: "spring", duration: 1}}
+          viewport={{margin: '-300px', once: true}}
+        >hellloooooooooooooooo</motion.h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
