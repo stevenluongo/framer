a1954b359a997fa4235bcc1199902ed0f7bfda5c
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-11-30 12:19:53 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 import { Input } from "./Input";
 import ParallaxText from "./ParallaxText";
 
-
-
 function App() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
@@ -12,39 +10,45 @@ function App() {
   return (
     <div>
       <div className="bg-[red] h-[100vh]">
-        <motion.h1 className="text-white text-5xl"
+        <motion.h1
+          className="text-white text-5xl"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-        >Hello world</motion.h1>
-      <motion.div
+        >
+          Hello world
+        </motion.h1>
+        <motion.div
           className="bg-[green] w-[200px] h-[200px]"
           animate={{ x, y, rotate }}
           transition={{ type: "spring" }}
         />
-              <div className="inputs">
-        <Input value={x} set={setX}>
-          x
-        </Input>
-        <Input value={y} set={setY}>
-          y
-        </Input>
-        <Input value={rotate} set={setRotate} min={-180} max={180}>
-          rotate
-        </Input>
-      </div>
+        <div className="inputs">
+          <Input value={x} set={setX}>
+            x
+          </Input>
+          <Input value={y} set={setY}>
+            y
+          </Input>
+          <Input value={rotate} set={setRotate} min={-180} max={180}>
+            rotate
+          </Input>
+        </div>
       </div>
       <div className="bg-[blue] h-[100vh]">
-      <motion.h1 className="text-white text-5xl"
+        <motion.h1
+          className="text-white text-5xl"
           initial={{ y: 30, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1  }}
-          transition={{ type: "spring", duration: 1}}
-          viewport={{margin: '-300px', once: true}}
-        >hellloooooooooooooooo</motion.h1>
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring", duration: 1 }}
+          viewport={{ margin: "-300px", once: true }}
+        >
+          hellloooooooooooooooo
+        </motion.h1>
       </div>
       <section className="bg-[purple] h-[100vh]">
-      <ParallaxText baseVelocity={-5}>Framer Motion</ParallaxText>
-      <ParallaxText baseVelocity={5}>Scroll velocity</ParallaxText>
-    </section>
+        <ParallaxText baseVelocity={-5}>Framer Motion</ParallaxText>
+        <ParallaxText baseVelocity={5}>Scroll velocity</ParallaxText>
+      </section>
     </div>
   );
 }
